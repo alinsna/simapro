5b27cf46430948dd51142c4e2c6c9730e2ff5a20
rulll <muhamadfahrulrazi@mail.ugm.ac.id>
2025-06-16 20:01:41 +0700

    bikin branch

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "stepdefinitions",
+        glue = "com.example.stepdefinitions",
         plugin = {"pretty", "html:target/cucumber-report.html"},
         monochrome = true
 )
